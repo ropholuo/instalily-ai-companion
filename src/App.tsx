2c3a56f8fa2ebbcf75dc0c66855d3c866344dc48
Javion Luo <78669543+ropholuo@users.noreply.github.com>
2023-10-24 23:28:13 -0400

    update url regex

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import api_key from "./apikey";
 
 function App() {
   const urlRegex =
-    /(http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(,]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)(?<!\))/g;
+    /(http|ftp|https):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:\/~+#-]*[\w@?^=%&\/~+#-])/g;
   const [messages, setMessages] = useState([
     { role: "assistant", content: "Hello! How can I assist you today?" },
   ]);
